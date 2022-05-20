bee82f162778e4eede1f0b6e1c6b91be10645059
Amy Martin <amymariemart@gmail.com>
2022-05-19 19:21:38 -0700

    Display appointments complete

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -143,7 +143,7 @@ storiesOf("Appointment", module)
   .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
   .add("Show", () => (
     <Show
-      student="Lydia Miller-Jones" 
+      student="Amy Martin" 
       interviewer={interviewer} 
       onEdit={action("onEdit")} 
       onDelete={action("onDelete")}
@@ -170,6 +170,22 @@ storiesOf("Appointment", module)
       onSave={action("onSave")}
       onCancel={action("onCancel")}
     />
+  ))
+  .add("Appointment Empty", () => (
+    <Fragment>
+      <Appointment id={1} time="4pm" />
+      <Appointment time="5pm" />
+    </Fragment>
+  ))
+  .add("Appointment Booked", () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="4pm"
+        interview={{ student: "Lydia Miller-Jones", interviewer }}
+      />
+      <Appointment time="5pm" />
+    </Fragment>
   ));
 
 
